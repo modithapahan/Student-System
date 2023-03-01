f9b90b7ca6449bb67e0ded893f2927ca873e646f
modithapahan <modithapahan0@gmail.com>
2023-03-02 00:19:47 +0530

    Complete the sign-in page google button

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import {FcGoogle} from 'react-icons/fc'
 
 const OAuth = () => {
-  return <div>OAuth</div>;
+  return (
+    <button className='flex justify-center items-center bg-red-700 text-white w-full px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'>
+      <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
+      Continue with Google
+    </button>
+  )
 };
 
 export default OAuth;
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -85,7 +85,7 @@ const SignIn = () => {
               </div>
               <button
               type="submit"
-              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase roundard shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
             >
               Sign in
             </button>
