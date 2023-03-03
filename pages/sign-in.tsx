09ea68b60cdb9bdf42b6e030801b42837fe6603c
modithapahan <modithapahan0@gmail.com>
2023-03-03 13:47:01 +0530

    Create sign in and sign up interfaces

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -3,7 +3,7 @@ import OAuth from "@/components/OAuth";
 import Head from "next/head";
 import Link from "next/link";
 import React, { useState } from "react";
-import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai';
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({
@@ -58,14 +58,28 @@ const SignIn = () => {
                 />
                 <div className="relative">
                   <input
-                    type={showPassword ? 'text' : 'password'}
+                    type={showPassword ? "text" : "password"}
                     placeholder="Enter Password"
                     value={password}
                     id="password"
                     onChange={onChange}
                     className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
                   />
-                  {showPassword ? (<AiFillEyeInvisible onClick={()=>setShowPassword((prevState)=>!prevState)} className={'absolute right-3 top-3 text-xl cursor-pointer'}/>) : <AiFillEye onClick={()=>setShowPassword((prevState)=>!prevState)} className={'absolute right-3 top-3 text-xl cursor-pointer'}/>}
+                  {showPassword ? (
+                    <AiFillEyeInvisible
+                      onClick={() => setShowPassword((prevState) => !prevState)}
+                      className={
+                        "absolute right-3 top-3 text-xl cursor-pointer"
+                      }
+                    />
+                  ) : (
+                    <AiFillEye
+                      onClick={() => setShowPassword((prevState) => !prevState)}
+                      className={
+                        "absolute right-3 top-3 text-xl cursor-pointer"
+                      }
+                    />
+                  )}
                 </div>
               </div>
               <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mt-4">
@@ -88,16 +102,16 @@ const SignIn = () => {
                 </p>
               </div>
               <button
-              type="submit"
-              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
-            >
-              Sign in
-            </button>
-            <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
-              <p className="text-center font-semibold mx-4">OR</p>
-            </div>
-            <OAuth />
-            </form>           
+                type="submit"
+                className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+              >
+                Sign in
+              </button>
+              <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+                <p className="text-center font-semibold mx-4">OR</p>
+              </div>
+              <OAuth />
+            </form>
           </div>
         </div>
       </section>
